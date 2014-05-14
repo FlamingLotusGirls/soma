c23ad6ad9f76c08a9fec282a6130beee15e8461b
Laura Kogler <lkogler@gmail.com>
2014-05-13 21:48:12 -0700

    Improvements to random phase effect layer

--- pier14/opc-client/effects/random_phase.py
+++ pier14/opc-client/effects/random_phase.py
@@ -11,78 +11,85 @@ class RandomPhaseLayer(EffectLayer):
         self.phase = [0]*model.numLEDs
         for i in range(model.numLEDs):
             self.phase[i] = random.random()
-        self.color = numpy.array((0.7, 0.9, 0.8))
-        self.randomness = 1
+        self.color = numpy.array((1, 0.5, 0.2))
+        self.lowerRandomness = 1
+        self.upperRandomness = 1
+        self.axonOn = False
         self.lifecycle = "start"
         self.chaseBlinks = -1
 
 
     def render(self, model, params, frame):
+        self.axonOn = False
         if self.lifecycle == "start":
             if params.buttonState[0] or params.buttonState[1]:
                 self.lifecycle = "buttonDown"
                 print "button down!"
             else:
-                self.computeDodecaValues(params.time, 1, model.lowerIndices, frame)
-                self.computeDodecaValues(params.time, 1, model.upperIndices, frame)
+                self.upperRandomness = 1
+                self.lowerRandomness = 1
         
         if self.lifecycle == "buttonDown":
             if not (params.buttonState[0] or params.buttonState[1]):
                 self.lifecycle = "decay"
                 print "button back up!"
             else:
-                self.randomness -= 0.005
-                if self.randomness < 0:
-                    self.randomness = 0
+                self.upperRandomness = 1
+                self.lowerRandomness -= 0.005
+                if self.lowerRandomness < 0:
+                    self.lowerRandomness = 0
                     self.lifecycle = "chase"
                     print "starting chase!"
-                    self.chaseStartTime = params.time
-                self.computeDodecaValues(params.time, self.randomness, model.lowerIndices, frame)
-                self.computeDodecaValues(params.time, 1, model.upperIndices, frame)
+                    self.cachedTime = params.time
 
         if self.lifecycle == "chase":
-
+            self.lowerRandomness = 0
             # synchronize start of chase cycle with minimum intensity of lower dodeca
             lowerDodecaCyclePosition = (params.time % self.period)
             if self.chaseBlinks < 0:
-                if lowerDodecaCyclePosition < 0.95:
+                if lowerDodecaCyclePosition < 0.5:
                     print "chase ready!"
                     self.chaseBlinks = 0
             elif self.chaseBlinks == 0:
-                if lowerDodecaCyclePosition > 0.95:
-                    self.chaseStartTime = params.time
+                if lowerDodecaCyclePosition > 0.5:
+                    self.cachedTime = params.time
                     print "launch the chase!"
                     self.chaseBlinks = 1
+            elif self.upperRandomness > 0:
+                self.axonOn = True
+                self.upperRandomness -= 0.002
             else:
-                self.computeAxonValues(params.time, model.axonIndices, frame)
+                self.lifecycle = "end"
+                self.cachedTime = params.time
 
-            self.computeDodecaValues(params.time, 0, model.lowerIndices, frame)
-            self.computeDodecaValues(params.time, 1, model.upperIndices, frame)
+        if self.lifecycle == "end": 
+            self.upperRandomness = 0
+            self.lowerRandomness = 0
+            if params.time - self.cachedTime > 3*self.period:
+                self.lifecycle = "decay"
+        
+        if self.lifecycle == "decay":
+            self.upperRandomness += 0.002
+            if self.upperRandomness > 1:
+                self.upperRandomness = 1
+            self.lowerRandomness += 0.002
+            if self.lowerRandomness > 1:
+                self.lowerRandomness = 1
 
-
-
-
-        # if params.buttonState[0]:
-        #     self.randomness -= 0.005
-        #     if self.randomness < 0:
-        #         self.randomness = 0
-        #         params.trigger = True
-        # else:
-        #     params.trigger = False
-        #     self.randomness += 0.01
-        #     if self.randomness > 1:
-        #         self.randomness = 1
-
-        # self.computeValues(params.time, self.randomness, model.lowerIndices, frame)
-        # self.computeValues(params.time, 1, model.upperIndices, frame)
-
+            if self.upperRandomness == 1 and self.lowerRandomness == 1:
+                self.lifecycle = "start"
+        
+        self.computeAxonValues(params.time, self.axonOn, model.axonIndices, frame)
+        self.computeDodecaValues(params.time, self.lowerRandomness, model.lowerIndices, frame)
+        self.computeDodecaValues(params.time, self.upperRandomness, model.upperIndices, frame)
 
     def computeDodecaValues(self, time, randomness, indices, frame): 
         for i in indices:
-            frame[i] += self.color * (math.sin(math.pi*(time*self.frequency + randomness*self.phase[i])))**2
+            frame[i] = self.color * (math.sin(math.pi*(time*self.frequency + randomness*self.phase[i])))**2
 
-    def computeAxonValues(self, time, indices, frame):
-        for i in indices:
-            frame[i] += self.color * (math.sin(math.pi*(time*self.frequency)))**2
+    def computeAxonValues(self, time, axonOn, indices, frame):
+        if axonOn:
+            for i in indices:
+                frame[i] = self.color * (math.sin(math.pi*(time*self.frequency)))**2
 
         
