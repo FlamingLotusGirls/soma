4fe6b8465eedc661b7b00779eabae83bce8cdd2b
Sarah Tappon <sarah.tappon@gmail.com>
2014-07-20 16:39:27 -0700

    add simple test pattern layer

--- pier14/opc-client/soma_test.py
+++ pier14/opc-client/soma_test.py
@@ -34,49 +34,6 @@ class SomaTestLayer(EffectLayer):
             frame[model.upperIndices] = 1
 
 
-# A sine wave in the X direction
-class SineWaveLayer(EffectLayer):
-    startTime = None
-
-    def __init__(self, period = 1.0, color = (1, 1, 1)):
-        self.period = period
-        self.color = numpy.array(color)
-
-    def render(self, model, params, frame):
-        if not self.startTime:
-            self.startTime = params.time
-
-        elapsedTime = (params.time - self.startTime)
-
-        # model.nodes[0] is the X coordinates in [0,1] range
-        # elapsedTime / self.period will increment by 1/period every second
-        radians = (model.nodes[:,0] + elapsedTime / self.period) * 2 * pi
-
-        cosines = numpy.cos(radians)
-
-        # the frame is Nx3 (R/G/B values for each of the N LEDs).
-        # reshape(-1,1) converts cosines from a 1D to an Nx1 2D array so it can be
-        # multplied with the color array to yield a Nx3 array
-        frame[:] = cosines.reshape(-1,1) * self.color
-
-
-# step through the LEDs in order of frame index
-class AddressTestLayer(EffectLayer):
-    def __init__(self):
-        self.index = 0
-        self.switchInterval = 0.5
-        self.lastSwitchTime = None
-        self.color = numpy.array([1,1,1])
-
-    def render(self, model, params, frame):
-        if not self.lastSwitchTime:
-            self.lastSwitchTime = params.time
-        elif params.time - self.lastSwitchTime > self.switchInterval:
-            self.index = (self.index + 1) % model.numLEDs
-            self.lastSwitchTime = params.time
-
-        frame[self.index] = self.color
-
 def main(screen):
 
     # master parameters, used in rendering and updated by playlist advancer thread
@@ -118,7 +75,8 @@ def main(screen):
         #     # ColorBlinkyLayer(),
         # ],
         [
-            AddressTestLayer(),
+            #AddressTestLayer(),
+            TestPatternLayer(),
             #ColorWave(model),
             # ColorWiper(model),
              # MultiplierLayer(ColorWave(model), ColorWiper(model)),
