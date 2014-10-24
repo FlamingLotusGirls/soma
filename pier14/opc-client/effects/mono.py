23df25ed34274a2e59562904e4d9240478d68454
Carolyn Wales <cswales@gmail.com>
2014-10-24 15:05:25 -0700

    adding monochrome effect layer for giants

---
+++ pier14/opc-client/effects/mono.py
@@ -0,0 +1,22 @@
+import math
+import random
+import numpy
+from colorsys import hsv_to_rgb, rgb_to_hsv
+
+from effectlayer import *
+
+class MonoLayer(EffectLayer):
+
+    # nb - 16 degrees is the hue for giants orange. Looks a little red to me.
+    def __init__(self, hue=16/float(360)):
+        self.hue = hue
+
+    def render(self, model, params, frame):
+        len = frame.size/3
+        myfloat = 16/float(360)
+        for i in range(0,len):
+            singleFrame = frame[i]
+            hsv = numpy.array(rgb_to_hsv(singleFrame[0], singleFrame[1], singleFrame[2]))
+            foo = numpy.array(hsv_to_rgb(16/360, 1.0, hsv[2]))
+            frame[i] = numpy.array(hsv_to_rgb(self.hue, 1.0, hsv[2]))
+            
