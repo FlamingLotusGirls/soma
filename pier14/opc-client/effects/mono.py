867154f4a94313dc60bb507b2c85708887c73992
Michael C. Toren <mct@toren.net>
2014-10-24 22:45:03 -0700

    More orange

--- pier14/opc-client/effects/mono.py
+++ pier14/opc-client/effects/mono.py
@@ -8,14 +8,13 @@ from effectlayer import *
 class MonoLayer(EffectLayer):
 
     # nb - 16 degrees is the hue for giants orange. Looks a little red to me.
-    def __init__(self, hue=16/float(360)):
+    def __init__(self, hue=30/float(360)):
         self.hue = hue
 
     def render(self, model, params, frame):
         len = frame.size/3
-        myfloat = 16/float(360)
         for i in range(0,len):
-            singleFrame = frame[i]
-            hsv = numpy.array(rgb_to_hsv(singleFrame[0], singleFrame[1], singleFrame[2]))
+            pixel = frame[i]
+            hsv = numpy.array(rgb_to_hsv(pixel[0], pixel[1], pixel[2]))
             frame[i] = numpy.array(hsv_to_rgb(self.hue, (hsv[1]+0.5) / 1.5, hsv[2]))
             
